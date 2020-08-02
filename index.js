6f32aa4dea6c75b60ef0cee6ee73aae7045d8c49
Emma Takács <delczegemma@gmail.com>
2020-08-03 00:03:14 +0200

    Add: Listening to changes

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 function createStore() {
-//The store should have four parts
-//1. the state
-//2. Get the state
-//3. Listen to stat changes
-//4. Update the state
+	//The store should have four parts
+	//1. the state
+	//2. Get the state
+	//3. Listen to stat changes
+	//4. Update the state
 
-let state
+	let state
+	let listeners = []
 
-const getState = () => state
+	const getState = () => state
 
-return {
-	getState
+	const subscribe = (listener) => {
+		listeners.push(listener)
+		return () => {
+			listeners = listeners.filter((l) => l !== listener)
+		}
+	}
+
+	return {
+		getState,
+		subscribe
+	}
+
 }
 
-}
+const store = createStore()
+
+store.subsrcibe(() => {
+	console.log('The new state is: ', store.getState())
+})
+
+const unsubscribe = store.subsrcibe(() => {
+	console.log('The store changed.')
+})
+
+unsubscribe()
