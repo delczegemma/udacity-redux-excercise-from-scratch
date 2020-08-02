b93100e11a1742bb21d613e660d734c678a038a1
Emma Takács <delczegemma@gmail.com>
2020-08-02 22:19:37 +0200

    feat: Start building the createStore function
    This factory function currently
    - takes in no arguments
    - sets up a local variable to hold the state
    - sets up a getState() function
    - returns an object that publicly exposes the getState() function

---
+++ index.js
@@ -0,0 +1,16 @@
+function createStore() {
+//The store should have four parts
+//1. the state
+//2. Get the state
+//3. Listen to stat changes
+//4. Update the state
+
+let state
+
+const getState = () => state
+
+return {
+	getState
+}
+
+}
