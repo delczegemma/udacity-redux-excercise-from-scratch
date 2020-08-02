3009b56cff4de500e9b340233f7785918ad6c49e
Emma Takács <delczegemma@gmail.com>
2020-08-03 01:59:57 +0200

    Feat: Add dispatch function to the state.
    Dispatch function is for to update the state inside of the store.
    In order to do it asks for the value of the reducer function and sets it as a new state.
    The dispatch function also calls all the registered listeners, because the state has potentially changed
    As todos was an app code, while store is our library code, we passed the former as a paramter order to handle later access issues

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-//The Reducer function is responsible to updating the state based on the current action
-//It takes alwas to arguments, the "state" and the "action"
-//This has to be a pure function to increase predictability.
-//So the function doesn't modify the current, instead with the concat() method it creates a new one,
-// in order to remain pure.
+// ---* Library Code *---//
 
-function todos(state = [], action) {
-	if(action.type === 'ADD_TODO') {
-		return state.concat([action.todo])
-	}
-	return state
-}
-
-function createStore() {
+function createStore(reducer) {
 	//The store should have four parts
 	//1. the state
 	//2. Get the state
@@ -30,21 +19,34 @@ function createStore() {
 		}
 	}
 
+	//Store dispatch function to update state inside the store (part 4)
+	const dispatch = (action) => {
+		state = reducer(state,action)
+		listeners.forEach((listener) =>listener())
+	}
+
 	return {
 		getState,
-		subscribe
+		subscribe,
+		dispatch
 	}
 
 }
 
+// ---* App Code *---//
+
+//The Reducer function is responsible to updating the state based on the current action
+//It takes alwas to arguments, the "state" and the "action"
+//This has to be a pure function to increase predictability.
+//So the function doesn't modify the current, instead with the concat() method it creates a new one,
+// in order to remain pure.
+function todos(state = [], action) {
+	if(action.type === 'ADD_TODO') {
+		return state.concat([action.todo])
+	}
+	return state
+}
+
 const store = createStore()
 
-store.subsrcibe(() => {
-	console.log('The new state is: ', store.getState())
-})
-
-const unsubscribe = store.subsrcibe(() => {
-	console.log('The store changed.')
-})
-
-unsubscribe()
+//const store = createStore(todos)
